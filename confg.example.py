37ff7f3b1222458d360d11268e79c18b0874a0c9
xu001186 <xu001186@gmail.com>
2021-11-21 11:14:16 +1100

    add example config

---
+++ confg.example.py
@@ -0,0 +1,14 @@
+# the mercernaries pickup order, in this case, the card 1,2 and 3 are picked.
+mercenaries = [1,2,3]
+# the spell order , in this case ,the card1 ,card2 and card3 are using the spell1 in the first round ,
+# in 2nd round the card1 is using the spell 3, and card 2 is using spell 2 and the card 3 is using the spell 2 . 
+# in 3rd round , the card1 ,card2 and card3 are using the spell 1 , and so on.  
+spells = [[1,1,2],[2,3,1] ]
+# # the bounty selection order, in this case , the 5th bounty is selected.
+bounty_no = 1
+# the mercenary collection selection order, in this case , the 3rd collection is selected
+mercenary_collection = 4
+# whether the bot traverses the map to find the mysterious vistor
+is_path_to_mysterious = True
+# Is the bot starts from bounty selection
+start_from_bounty = False
